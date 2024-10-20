efabb2a326a43d3949f2fcc9628a8c82f1493312
Dohyun Kim <0704dohyun@gmail.com>
2024-10-21 01:19:30 +0900

    chore(config): fix environment varible to normal string

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.SUPABASE_HOST,
+        hostname: "njsbftuzvmqfrvqlzaug.supabase.co",
       },
     ],
   },
